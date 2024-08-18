9714e21fc05e0e504de28c44f430d3e43889e0b8
Garett Robson <info@garettrobson.co.uk>
2024-08-18 17:00:53 +0100

    refactor: Move command configuration from main file into LintApplication

--- php-commit-lint.php
+++ php-commit-lint.php
@@ -3,14 +3,7 @@
 include 'vendor/autoload.php';
 
 use GarettRobson\PhpCommitLint\Application\LintApplication;
-use GarettRobson\PhpCommitLint\Command\LintMessageCommand;
 
 $application = new LintApplication();
-
-$application->addCommands([
-    new LintMessageCommand()
-]);
-
-$application->setDefaultCommand('message:lint', true);
 
 $application->run();
--- src/Application/LintApplication.php
+++ src/Application/LintApplication.php
@@ -5,7 +5,17 @@ declare(strict_types=1);
 namespace GarettRobson\PhpCommitLint\Application;
 
 use Symfony\Component\Console\Application;
+use GarettRobson\PhpCommitLint\Command\LintMessageCommand;
 
 class LintApplication extends Application
 {
+
+    public function __construct()
+    {
+        parent::__construct('git-commit-lint', '0.0.0');
+        $this->addCommands([
+            new LintMessageCommand()
+        ]);
+        $this->setDefaultCommand('message:lint', true);
+    }
 }

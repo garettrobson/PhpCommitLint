be085e362ae795af6e90a082e3f39815a35f02d9
Garett Robson <info@garettrobson.co.uk>
2024-08-22 17:24:44 +0100

    test(command): Add test for config:setup command

---
+++ tests/Command/ConfigSetupCommandTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GarettRobson\PhpCommitLint\Tests\Command;
+
+use GarettRobson\PhpCommitLint\Application\PhpCommitLintApplication;
+use GarettRobson\PhpCommitLint\Command\ConfigCommand;
+use GarettRobson\PhpCommitLint\Command\ConfigSetupCommand;
+use GarettRobson\PhpCommitLint\Command\LintCommand;
+use GarettRobson\PhpCommitLint\Message\ConventionalCommitsMessageParser;
+use GarettRobson\PhpCommitLint\Message\Message;
+use GarettRobson\PhpCommitLint\Message\MessageParser;
+use GarettRobson\PhpCommitLint\Message\PatternFileMessageParser;
+use GarettRobson\PhpCommitLint\Validation\LineLengthRule;
+use GarettRobson\PhpCommitLint\Validation\PropertyExistenceRule;
+use GarettRobson\PhpCommitLint\Validation\PropertyRegexRule;
+use GarettRobson\PhpCommitLint\Validation\PropertySetRule;
+use GarettRobson\PhpCommitLint\Validation\Validator;
+use GarettRobson\PhpCommitLint\Validation\ValidatorConfiguration;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * @internal
+ */
+#[CoversClass(LintCommand::class)]
+#[CoversClass(PhpCommitLintApplication::class)]
+#[CoversClass(ConventionalCommitsMessageParser::class)]
+#[CoversClass(MessageParser::class)]
+#[CoversClass(PatternFileMessageParser::class)]
+#[CoversClass(Message::class)]
+#[CoversClass(Validator::class)]
+#[CoversClass(LineLengthRule::class)]
+#[CoversClass(PropertyExistenceRule::class)]
+#[CoversClass(PropertySetRule::class)]
+#[CoversClass(ConfigCommand::class)]
+#[CoversClass(ValidatorConfiguration::class)]
+#[CoversClass(PropertyRegexRule::class)]
+#[CoversClass(ConfigSetupCommand::class)]
+class ConfigSetupCommandTest extends TestCase
+{
+    public function testExecuteWithValidMessage(): void
+    {
+        $application = new PhpCommitLintApplication();
+
+        $command = $application->find('config:setup');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'target-directory' => __DIR__.'/res/tmp',
+            '--rule-set' => ['formatting50-72'],
+            '--yes' => true,
+        ]);
+
+        $commandTester->assertCommandIsSuccessful();
+        $output = $commandTester->getDisplay();
+
+        $expected = file_get_contents(__DIR__.'/res/test/.php-commit-lint-reference.json');
+        $actual = file_get_contents(__DIR__.'/res/tmp/.php-commit-lint.json');
+
+        $this->assertSame($expected, $actual);
+    }
+}

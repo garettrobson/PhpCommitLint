a79798e7e8621f7eeffa5a50fda368539b785419
Garett Robson <info@garettrobson.co.uk>
2024-08-23 16:46:49 +0100

    refactor(validation): Consistent variable names

--- src/Validation/PropertyExistenceRule.php
+++ src/Validation/PropertyExistenceRule.php
@@ -10,13 +10,13 @@ class PropertyExistenceRule extends PropertyRule
 {
     public function __construct(
         protected string $property,
-        protected bool $exists = true,
+        protected bool $positiveCheck = true,
         protected string $errorMessage = 'Required property %s missing',
     ) {}
 
     public function performValidation(Message $message): self
     {
-        if ($this->exists ^ $message->has($this->property)) {
+        if ($this->positiveCheck ^ $message->has($this->property)) {
             $this->addError(
                 $this->errorMessage,
                 $this->property
